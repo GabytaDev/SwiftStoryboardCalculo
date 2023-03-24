2057be4b7f4e9609c48dea25d691349bf31dba15
GabytaDev <gabyta.loyola@gmail.com>
2023-03-24 12:13:23 -0300

    first commit

--- calculoPorcentaje/ViewController.swift
+++ calculoPorcentaje/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultado: UILabel!
+    @IBOutlet weak var resultadoDescuento: UILabel!
+    @IBOutlet weak var cantidadTexfield: UITextField!
+    @IBOutlet weak var porcentajeTexfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+       NotificationCenter.default.addObserver(self, selector: #selector(tecladoDown), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-
+    
+    @objc func tecladoUp () {
+        let sizeScreen = UIScreen.main.nativeBounds.height
+        if sizeScreen == 1334 {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y = -50
+            }
+        }
+    }
+    
+    @objc func tecladoDown () {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    //para quese vaya el teclado tocando cualquier parte de la pantalla toucheBegan
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcularButton(_ sender: UIButton) {
+        guard let cantidad = cantidadTexfield.text else { return}
+        guard let porcentaje = porcentajeTexfield.text else {return}
+        let cant = (cantidad as NSString).floatValue
+        let porciento = (porcentaje as NSString).floatValue
+        
+        let desc = cant * porciento/100
+        let res = cant - desc
+        
+        resultado.text = "$\(res)"
+        resultadoDescuento.text = "$\(desc)"
+        self.view.endEditing(true)
+        
+    }
+    
+    @IBAction func limpiarButton(_ sender: UIButton) {
+        cantidadTexfield.text = ""
+        porcentajeTexfield.text = ""
+        resultado.text = "0,00"
+        resultadoDescuento.text = "0,00"
+    }
 }
 
